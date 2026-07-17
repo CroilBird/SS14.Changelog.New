b6e9c247b131aea4fa35b7240da2f9798eef7a6a
Crude Oil <crude@croil.net>
2026-07-17 13:25:30 +0200

    initial commit

---
+++ TestChangelog/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.CommandLine;
+
+namespace TestChangelog
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Option<string> fileOption = new("--")
+            {
+                Description = "The file to read and display on the console"
+            };
+        }
+    }
+}
